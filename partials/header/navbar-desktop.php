67851b72904ec4d02c51ad09006db1b327573862
Joan <papamundodev@gmail.com>
2025-03-27 11:44:59 +0100

    nav desktop dropdown over , sass: colors and typo in separated files

--- partials/header/navbar-desktop.php
+++ partials/header/navbar-desktop.php
@@ -23,16 +23,16 @@ $theme_location = $args['theme_location'] ?? "header";
 
         <!-- desktop dropdown -->
 
-        <div name="navmenu-header-dropdown-<?=$theme_location?>" class="dropdown dropdown-over dropdown-<?=$theme_location === 'footer' ? "footer" : "header" ?>">
-            <a href="<?=$item['url']?>" class="nav-link"><?=$item['title']?></a>
+        <div tabindex="0" name="navmenu-header-dropdown-<?=$theme_location?>" class="dropdown dropdown-over dropdown-<?=$theme_location === 'footer' ? "footer" : "header" ?>">
+            <span class="nav-link"><?=$item['title']?></span>
             <svg class="plus-icon-svg" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
               <path id="path-1" d="M32 128C32 123.582 35.5817 120 40 120L216 120C220.418 120 224 123.582 224 128V128C224 132.418 220.418 136 216 136H128H40C35.5817 136 32 132.418 32 128V128Z" />
               <path id="path-2" d="M128 224C123.582 224 120 220.418 120 216V40C120 35.5817 123.582 32 128 32V32C132.418 32 136 35.5817 136 40V128V216C136 220.418 132.418 224 128 224V224Z" />
             </svg>  
         
           <ul class="dropdown-menu">
-            <?php foreach($item['children'] as $child): ?>
-              <li class="<?= \Theme_base\Base::get_active_class($child) ?>">
+            <?php $i = 0; foreach($item['children'] as $child): ?>
+              <li  class="<?= \Theme_base\Base::get_active_class($child) ?>">
                 <a  class="nav-link"
                 href="<?=$child['url']?>"
                 title="<?=$child['title']?>"
@@ -40,7 +40,7 @@ $theme_location = $args['theme_location'] ?? "header";
                 rel="<?= $child['target'] === '_blank' ? 'noopener' : '' ?>"
                 ><?=$child['title']?></a>
               </li>
-            <?php endforeach; ?>
+              <?php $i++; endforeach; ?>
           </ul>
         </div>
       <?php endif; ?>
